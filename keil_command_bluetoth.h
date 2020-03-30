0df6909fccd49303ae687a788e9a9381185858ca
Sofia <gn17o@yandex.ru>
2020-03-30 22:26:17 +0300

    keil comma

---
+++ keil_command_bluetoth.h
@@ -0,0 +1,11 @@
+#ifdef __cplusplus
+extern "C" {
+#include <reg51.h>
+#include "UART_H_file.h"
+#include <REG_H_for_keil.h>
+#define KEIL_TO_PORT 0xff
+#define TO_SERIAL_PORT 0x00
+#define TOPORT_OUTPUT_A 0x4006_0000
+#define TOPORT_OUTPUT_B 0x4005_8000
+#endif
+}
